5c5559422c414c0a1f702b33f67bebb41d2c6ac9
Rushikesh Patil <patilrushi527@gmail.com>
2026-03-27 23:54:50 +0530

    Implement selection sort algorithm in d8_selection_sort.py

---
+++ d8_selection_sort.py
@@ -0,0 +1,12 @@
+# Day 8: Selection Sort
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_index = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    return arr
+arr = list(map(int, input("Enter elements: ").split()))
+print(selection_sort(arr))
